b941abf04573078a2729d56e3495bb487bdf3633
will-Liu-android <will.liu@wolaidai.com>
2016-10-17 17:51:25 +0800

    新的布局

--- app/src/main/java/quan/will/wheelpicker/MainActivity.java
+++ app/src/main/java/quan/will/wheelpicker/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sample_wheel_picker);
+        setContentView(R.layout.scroller_layout_sample);
     }
 }
